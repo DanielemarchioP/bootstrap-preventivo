4c3e6cab781fc3ff66e82fbf8be375b6adb79c69
DanieleM <danielemarchio709@gmail.com>
2024-04-05 23:54:59 +0200

    add: esercizio corretto con metodo includes

--- js/app.js
+++ js/app.js
@@ -9,8 +9,10 @@
 
 
 const form = document.getElementById('calcolo-prezzo')
+const codiceSconto = ['YHDNU32','JANJC63','PWKCN25','SJDPO96','POCIE24']
 
 form.addEventListener('submit', calcoloPreventivo)
+
 
 
 function calcoloPreventivo(e){
@@ -25,22 +27,26 @@ function calcoloPreventivo(e){
      console.log(totalePreventivo)
 
      let prezzoDiscount = 0
-     if( discount === 'YHDNU32'|| discount === 'JANJC63'|| discount === 'PWKCN25'|| discount === 'SJDPO96'|| discount === 'POCIE24'){
-        prezzoDiscount = totalePreventivo * 0.25
-        totalePreventivo = totalePreventivo - prezzoDiscount
-        console.log(prezzoDiscount)
-        console.log(totalePreventivo)
-     }else if(discount === ''){
-        console.log(totalePreventivo)
-     }
-     else{
-        console.log(totalePreventivo)
-        alert('codice promozionale non valido')
-     }
 
-     const prezzoFinalestrg = `<h2>&euro;${totalePreventivo}</h2>`
-     const h1ElementPrezzoFinale = document.getElementById('prezzo-finale')
+      if( codiceSconto.includes(discount)) {
+         prezzoDiscount = totalePreventivo * 0.25
+         totalePreventivo = totalePreventivo - prezzoDiscount
+         console.log(prezzoDiscount)
+         console.log(totalePreventivo)
 
-    h1ElementPrezzoFinale.innerHTML = prezzoFinalestrg
+      }else if(discount === ''){
+         console.log(totalePreventivo)
 
+      }else{
+         console.log(totalePreventivo)
+         alert('codice promozionale non valido')
+      }
+   
+ 
+      const prezzoFinalestrg = `&euro;${parseFloat(totalePreventivo).toFixed(2)}`
+      const h1ElementPrezzoFinale = document.getElementById('prezzo-finale')
+ 
+     h1ElementPrezzoFinale.innerHTML = prezzoFinalestrg
+     
+     
 }
